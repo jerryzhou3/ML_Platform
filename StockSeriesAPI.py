2479d58a98f191ec77802f29dd96986fbea86197
Yeju Zhou <jerryzhou3@gmail.com>
2021-01-27 11:01:36 +0800

    condense API into one function

--- StockSeriesAPI.py
+++ StockSeriesAPI.py
@@ -21,15 +21,14 @@ with open('api_key', 'r') as f:
 # In[3]:
 
 
-def TIME_SERIES_DAILY_ADJUSTED(symbol, outputsize = 'compact', datatype = 'json', apikey = API_KEY):
+def TIME_SERIES_DAILY_ADJUSTED_TO_CSV(symbol, outputsize = 'compact', datatype = 'json', apikey = API_KEY):
     api_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&outputsize={outputsize}&datatype={datatype}&apikey={apikey}'
     response = requests.get(api_url)
     json_content = json.loads(response.content)
-    return json_content['Time Series (Daily)']
-
-def TIME_SERIES_TO_CSV(time_series, columns = ['open', 'high', 'low', 'close', 'adjusted close', 'volume', 'dividend amount', 'split coefficient']):
+    time_series = json_content['Time Series (Daily)']
     df = pd.DataFrame.from_dict(time_series, orient = 'index')
     df.rename(columns = {'1. open': 'open', '2. high': 'high', '3. low': 'low', '4. close': 'close', '5. adjusted close': 'adjusted close', '6. volume': 'volume', '7. dividend amount': 'dividend amount', '8. split coefficient': 'split coefficient'}, inplace = True)
+    columns = ['open', 'high', 'low', 'close', 'adjusted close', 'volume', 'dividend amount', 'split coefficient']
     df = df[columns]
     return df
 
