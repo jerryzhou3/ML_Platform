ed3ba01ea97e3cd5590662a97d041bd96d32871f
Yeju Zhou <jerryzhou3@gmail.com>
2021-02-06 16:42:58 +0800

    multiservice and health check

--- Server.py
+++ Server.py
@@ -1,10 +1,11 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 import requests
 import dill
 import json
 import pandas as pd
+import time
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
 db_file = "sqlite:///{}".format(os.path.join(project_dir, "database.db"))
@@ -27,7 +28,7 @@ class Stock(db.Model):
     split = db.Column(db.Float, unique = False, nullable = False)
 
     def __repr__(self):
-		return "<Code: {}, Open: {}, High: {}, Low: {}, Close: {}, Adjusted Close: {}, Volume: {}, Dividend Amount: {}, Split Coefficient: {}>"\
+        return "<Code: {}, Open: {}, High: {}, Low: {}, Close: {}, Adjusted Close: {}, Volume: {}, Dividend Amount: {}, Split Coefficient: {}>"\
             .format(self.code, self.open, self.high, self.low, self.close, self.adjusted_close, self.volume, self.dividend, self.split)
 
 class Coin(db.Model):
@@ -37,6 +38,11 @@ class Coin(db.Model):
 
     def __repr__(self):
         return "<Asset: {}, USDC Rate: {}>".format(self.code, self.exchange_rate_usdc)
+
+API_KEY = None
+with open('api_key', 'r') as f:
+    json_content = f.read()
+    API_KEY = json.loads(json_content)['alphavantage']
 
 def TIME_SERIES_DAILY_ADJUSTED_TO_JSON(symbol, outputsize = 'compact', datatype = 'json', apikey = API_KEY):
     api_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&outputsize={outputsize}&datatype={datatype}&apikey={apikey}'
@@ -49,12 +55,16 @@ def TIME_SERIES_DAILY_ADJUSTED_TO_JSON(symbol, outputsize = 'compact', datatype 
     df = df[columns]
     return df.to_json(orient = 'index')
 
-serialize = dill.dumps(TIME_SERIES_DAILY_ADJUSTED_TO_JSON)
+TIME_SERIES_DAILY_ADJUSTED_TO_JSON_SERIALIZED = dill.dumps(TIME_SERIES_DAILY_ADJUSTED_TO_JSON)
 
-API_KEY = None
-with open('api_key', 'r') as f:
-    json_content = f.read()
-    API_KEY = json.loads(json_content)['alphavantage']
+SERVICES = {"ec2-18-167-84-46.ap-east-1.compute.amazonaws.com": {"port": 80, "task_count": 0, "healthy": 1}, \
+    "ec2-18-162-143-65.ap-east-1.compute.amazonaws.com": {"port": 80, "task_count": 0, "healthy": 1}}
+TASK_TABLE = {"ec2-18-167-84-46.ap-east-1.compute.amazonaws.com": [], "ec2-18-162-143-65.ap-east-1.compute.amazonaws.com": []}
+
+MAX_TASKS_PER_SERVICE = 2
+MAX_TASKS_TOTAL = 4
+CURRENT_TASK_COUNT = 0
+
 
 @app.route("/", methods = ["GET", "POST"])
 def home():
@@ -62,12 +72,60 @@ def home():
     coins = Coin.query.all()
     return render_template('home.html', stocks = stocks, coins = coins)
 
-@app.route("/query", methods = ["GET", "POST"])
-def query():
+@app.route("/task_assigner", methods = ["GET", "POST"])
+def task_assigner():
     for asset in request.args.get('asset'):
-        params = {'symbol': asset, 'apikey': API_KEY, 'token': 'test_token'}
-        data = requests.post(url = "http://127.0.0.1:5000/run_task", params = params, data = serialize)
-        # TO DO #
-        ## ADD TO DATABASE ##
-    
+        # parse task
+        params = {'asset': asset, 'apikey': API_KEY, 'token': 'test_token'}
+        service_to_assign = None
+        min_task_count = float('inf')
+        # find the least busy server
+        for service, info in SERVICES.items():
+            if info['task_count'] < min_task_count and info['healthy']:
+                min_task_count = info['task_count']
+                service_to_assign = service
+        # post task to service
+        response = requests.post(url = f"http://{service_to_assign}:{SERVICES[service_to_assign]['port']}/task_assigner", \
+            params = params, data = TIME_SERIES_DAILY_ADJUSTED_TO_JSON_SERIALIZED)
+        # update task table and counter if task assignment successful
+        if response.text == 'success':
+            CURRENT_TASK_COUNT += 1
+            service_to_assign['task_count'] += 1
+            TASK_TABLE[service_to_assign].append(asset) 
 
+@app.route("/task_result_receiver", methods = ["GET", "POST"])
+def task_result_receiver():
+    # get variables and data
+    CURRENT_TASK_COUNT -= 1
+    dns = request.args.get('DNS')
+    SERVICES[dns]['task_count'] -= 1
+    asset = request.args.get('asset')
+    TASK_TABLE[dns].remove(asset)
+    date = request.args.get('date')
+    data = request.json
+    # insert data to database
+    stock_data = Stock(code = asset, date = date, open = data['open'], high = data['high'], low = data['low'], close = data['close'], \
+        adjusted_close = data['adjusted close'], volume = data['volume'], dividend = data['dividend amount'], split = data['split coefficient'])
+    db.session.add(stock_data)
+    db.session.commit()
+    return "success", 200
+    
+@app.route("/health_checker", methods = ["GET"])
+def health_checker():
+    status = {}
+    for service in SERVICES:
+        try:
+            response = requests.get(f"http://{service}:{SERVICES[service]['port']}/health_checker")
+            if response.text == "success":
+                SERVICES[service]['health'] = 1
+                status[service] = "healthy"
+        except:
+            SERVICES[service]['health'] = 0
+            status[service] = "not healthy"
+    return jsonify({"service status": status}), 200
+            
+
+
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 5001, debug = True)
--- Service.py
+++ Service.py
@@ -3,20 +3,48 @@ import json
 import requests
 import pandas as pd
 import dill
+import multiprocessing as mp
+import time
 
 app = Flask(__name__)
 
 TOKEN = 'test_token'
 
-@app.route('/run_task', methods = ['GET', 'POST'])
-def run_task():
+execute_queue = mp.Queue(10)
+result_queue = mp.Queue(10)
+
+def execute_task(exe_q, res_q):
+    while True:
+        task = exe_q.get()
+        parser = dill.loads(task['parser'])
+        data = parser(symbol = task['asset'], apikey = task['apikey'])
+        res_q.put(data)
+        
+
+@app.route('/task_receiver', methods = ['GET', 'POST'])
+def task_receiver():
     token = request.args.get('token')
     if token != TOKEN:
-        return jsonify({'status code': 401})
-    parser = dill.loads(request.data)
-    data = parser(symbol = request.args.get('symbol'), apikey = request.args.get('apikey'))
-    return data
-    # return jsonify({'params': params, 'parser': parser, 'token': token})
+        return "wrong token", 401
+    
+    try:
+        task = {"parser": request.data, "asset": request.args.get('asset'), "apikey": request.args.get('apikey')}
+        execute_queue.put(task, timeout = 5)
+        # p = mp.Process(target = execute_queue, args = (execute_queue, result_queue))
+        # p.start()
+        return "success", 200
+    except mp.Queue.Full:
+        return "timeout", 408
+    
+
+# @app.route('/task_executor')
+# def task_executor():
+#     while not execute_queue.empty():
+
+@app.route("/health_checker")
+def health_checker():
+    return "success", 200
+
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port = 5000, debug = True)
+    app.run(host = "0.0.0.0", port = 80, debug = True)
